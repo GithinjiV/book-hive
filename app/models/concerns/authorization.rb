35896ec6a10645ccb4acfb44e8d79a7495635146
GithinjiV <githinjigicheru146@gmail.com>
2025-02-11 12:54:12 +0300

    Use not found instead of forbidden for unauthorized access

--- app/models/concerns/authorization.rb
+++ app/models/concerns/authorization.rb
@@ -1,10 +1,10 @@
 module Authorization
   private
   def ensure_can_administer
-    head :forbiddden unless Current.user.can_administer?
+    head :not_found unless Current.user.can_administer?
   end
 
   def ensure_current_user
-    head :forbidden unless @user.current?
+    head :not_found unless @user.current?
   end
 end
