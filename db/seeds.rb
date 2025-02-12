06a04171b1dad04b1d101aeb77b2069f8ced12f1
GithinjiV <githinjigicheru146@gmail.com>
2025-02-12 06:33:18 +0300

    Generate book data in development with faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+puts "Clearing existing books..."
+Book.destroy_all
+
+puts "Creating books..."
+6.times do |i|
+  Book.create!(
+    title: Faker::Book.unique.title,
+    author: Faker::Book.author,
+    isbn: Faker::Code.isbn,
+  )
+  print "." if (i + 1) % 10 == 0
+end
+
+puts "\nCreated #{Book.count} books!"
