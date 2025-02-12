f87bb59d4f81d94c317fb836be3c1585a6f0a524
GithinjiV <githinjigicheru146@gmail.com>
2025-02-12 08:28:23 +0300

    Show borrowed books to librarian in their profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
     @user = Current.user
     @current_borrowings = @user.current_borrowings
     @overdue_books = @user.overdue_books
+    @books = Book.all if @user.can_administer?
   end
 end
