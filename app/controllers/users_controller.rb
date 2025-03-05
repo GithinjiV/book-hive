c878c2f18cc7cac3840c65d0a76b1ef24e5064ef
GithinjiV <githinjigicheru146@gmail.com>
2025-03-05 22:17:55 +0300

    Fix user views to only show current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  include Authorization
   before_action :set_user
   before_action :load_admin_resources, if: :user_can_administer
   def profile
@@ -12,6 +13,7 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find_by!(username: params[:username])
+    ensure_current_user
   end
 
   def load_admin_resources
