a72110c82b01fd731b8c95e24f9c613acf27ee7b
GithinjiV <githinjigicheru146@gmail.com>
2025-03-08 21:17:48 +0300

    Only use globals where necessary

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def profile
     @current_borrowings = @user.current_borrowings
     @overdue_books = @user.overdue_books
-    @borrowings = Current.user.circulation_records.includes(:book).order(created_at: :desc)
+    @borrowings = @user.circulation_records.includes(:book).order(created_at: :desc)
     @past_borrowings = @user.circulation_records.returned
   end
 
