065c6c38a1d2f9a03f910b33b564b25b8c74e5ba
GithinjiV <githinjigicheru146@gmail.com>
2025-03-10 23:05:49 +0300

    Update user time zone

--- app/models/current.rb
+++ app/models/current.rb
@@ -1,4 +1,8 @@
 class Current < ActiveSupport::CurrentAttributes
   attribute :session
   delegate :user, to: :session, allow_nil: true
+
+  # def user(=user)
+  #   Time.zone = user.time_zone
+  # end
 end
